2c59941d4cd5c6cd1acc78225b1bf1ddbcfa26f9
lamilekan263 <lamilekan263@gmail.com>
2025-02-19 00:33:02 +0100

    layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main >
+            <h1 className="text-4xl font-bold">About Page</h1>
+            <div className='mt-5'>Welcome to the about page</div>
+        </main>
+    )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome to GPT chat</h1>
-
     </main>
   );
 }
